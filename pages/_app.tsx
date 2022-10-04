467f0c6a0348d933370f6e9334ce184dffb427e3
GEON1999 <phgst12@gmail.com>
2022-10-05 05:28:49 +0900

    sandbox error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,8 +15,10 @@ export default function App({ Component, pageProps }: any) {
           fetch(url).then((response) => response.json()),
       }}
     >
-      <ValidateUser />
-      <Component {...pageProps} />
+      <div>
+        <ValidateUser />
+        <Component {...pageProps} />
+      </div>
     </SWRConfig>
   );
 }
